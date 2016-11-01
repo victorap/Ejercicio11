18341f4b2d05e035e32a94a42c4d6fd2550e24ed
victorap <victorap909@hotmail.com>
2016-11-01 11:52:10 +0100

    Archivos

---
+++ app/src/main/java/com/example/victor/ejercicio11/Layout_1.java
@@ -0,0 +1,13 @@
+package com.example.victor.ejercicio11;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Layout_1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_layout_1);
+    }
+}
