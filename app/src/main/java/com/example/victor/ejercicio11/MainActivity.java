2256d24c69ce5484276bcb01ff38be9303f070ac
victorap <victorap909@hotmail.com>
2016-11-01 11:58:35 +0100

    Primer commit

--- app/src/main/java/com/example/victor/ejercicio11/MainActivity.java
+++ app/src/main/java/com/example/victor/ejercicio11/MainActivity.java
@@ -3,7 +3,7 @@ package com.example.victor.ejercicio11;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class Layout_1 extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
